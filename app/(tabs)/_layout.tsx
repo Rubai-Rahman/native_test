10718431e2fc7335b614cfbdabb722059dd2addc
Rubai Rahman <rubairahman1@gmail.com>
2025-11-20 12:33:06 +0600

    initial structure understood

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,27 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        tabBarActiveBackgroundColor: "coral",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="home"
+              size={24}
+              color={focused ? "white" : "black"}
+            />
+          ),
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      <Tabs.Screen name="login" options={{ title: "Login" }} />
     </Tabs>
   );
 }
